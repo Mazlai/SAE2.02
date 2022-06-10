a25025cfb4578c9b0cd8740198674700626f19f3
Mazlai | マイ <57178136+Mazlai@users.noreply.github.com>
2022-06-10 17:42:54 +0200

    Update : main.c

--- test.c
+++ test.c
@@ -1,5 +1,16 @@
 #include <stdio.h>
-#include "solution.h"
+#include <stdlib.h>
+#include <string.h>
+#include "solution.c"
+#include "solution2.c"
+
 int main() {
-    printf("%s\n",erase("Cou cou  J M  B"));
+    
+    //Premier test
+    printf("%s\n",erase("Cou cou  J M  B"));  
+    //Second test
+    char chaine[] = "Cou cou  J M  B";
+    printf("%s\n",erase2(chaine));
+    
+    return 0;
 }
