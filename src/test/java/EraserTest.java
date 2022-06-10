4c19584df89a19ce3d227896cae67d5998bc570f
Mazlai | マイ <57178136+Mazlai@users.noreply.github.com>
2022-06-10 23:58:44 +0200

    Update : ErasterTest.java

--- src/test/java/EraserTest.java
+++ src/test/java/EraserTest.java
@@ -3,6 +3,7 @@ import static org.junit.Assert.assertEquals;
 import org.junit.runners.JUnit4;
 
 import eraser.Eraser;
+import eraser.Eraser2;
 
 public class EraserTest {
     @Test
@@ -12,5 +13,11 @@ public class EraserTest {
         assertEquals("OK", Eraser.erase("OK"));
         assertEquals("Coucou  JM  B", Eraser.erase("Cou cou  J M  B"));
         assertEquals("", Eraser.erase(""));
+        
+        assertEquals("666,thenumberofthebeast", Eraser2.erase("666, the number of the beast"));
+        assertEquals("06   07651970", Eraser2.erase("06   07 65 19 70 "));
+        assertEquals("OK", Eraser2.erase("OK"));
+        assertEquals("Coucou  JM  B", Eraser2.erase("Cou cou  J M  B"));
+        assertEquals("", Eraser2.erase(""));
     }
 }
