8954b2f4f6e082849d50a750170076b196606550
Mazlai | マイ <57178136+Mazlai@users.noreply.github.com>
2022-06-10 17:59:56 +0200

    Update : solution.py

--- solution.py
+++ solution.py
@@ -1,4 +1,15 @@
 # return string without spaces
 def erase(cc):
-
-    pass
+    """
+    Removes only single spaces from given string.
+    :param text : string to edit
+    :return : edited string
+    """
+    listedText = list(cc)    
+    for i, char in enumerate(listedText):
+        tailleText = len(listedText)
+        if (char == " " and tailleText == 1) or (char == " "
+          and listedText[max(i-1, int(tailleText > 1))] != " " 
+          and listedText[min(i+1, tailleText-int(tailleText > 1)*2)] != " "):
+            del listedText[i]
+    return "".join(char for char in listedText)
