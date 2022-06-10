7b16efc981f5c6071f4d697de444df58029244d8
Mazlai | マイ <57178136+Mazlai@users.noreply.github.com>
2022-06-10 23:49:40 +0200

    Create : Erase2.java

---
+++ src/main/java/eraser/Eraser2.java
@@ -0,0 +1,38 @@
+package eraser;
+
+public class Eraser2 {
+    /** Permet de modifier la chaîne de caractère renseignée en paramètre
+     * @param IN str : la chaîne de caractère
+     * @return la nouvelle chaîne de caractère avec les traitements effectués
+     **/ 
+    public static String erase(String str) {
+        //throw new UnsupportedOperationException("Unimplemented");
+        
+        String s1 = "";
+        
+        //Vérification se réalisant en début de chaîne de caractère
+        if(str.length() > 1) {
+            if(str.charAt(0) != ' ' || str.charAt(0) == ' ' && str.charAt(1) == ' ') {
+                s1 += String.valueOf(str.charAt(0));
+            }
+        }
+        
+        //Vérification se réalisant en milieu de chaîne de caractère
+        for(int i = 1; i < str.length() - 1 ; i++) {
+            if(str.charAt(i) != ' ' || (str.charAt(i-1) == ' ' || str.charAt(i+1) == ' ')) {
+                s1 += String.valueOf(str.charAt(i));
+            }
+        }
+        
+        //Vérification se réalisant en fin de chaîne de caractère
+        if(str.length() > 1) {
+            if(str.charAt(str.length() - 1) != ' ' || str.charAt(str.length() - 2) == ' ') {
+                s1 += String.valueOf(str.charAt(str.length() - 1));        
+            }
+        }
+        
+            
+        return s1;
+        
+    }
+}
