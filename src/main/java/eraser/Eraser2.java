c609c028872fe2cb80b82a7c75c3a2d529f19044
Mazlai | マイ <57178136+Mazlai@users.noreply.github.com>
2022-06-10 23:54:52 +0200

    Update (variable) : Erase2.java

--- src/main/java/eraser/Eraser2.java
+++ src/main/java/eraser/Eraser2.java
@@ -8,30 +8,29 @@ public class Eraser2 {
     public static String erase(String str) {
         //throw new UnsupportedOperationException("Unimplemented");
         
-        String s1 = "";
+        String newstring = "";
         
         //Vérification se réalisant en début de chaîne de caractère
         if(str.length() > 1) {
             if(str.charAt(0) != ' ' || str.charAt(0) == ' ' && str.charAt(1) == ' ') {
-                s1 += String.valueOf(str.charAt(0));
+                newstring += String.valueOf(str.charAt(0));
             }
         }
         
         //Vérification se réalisant en milieu de chaîne de caractère
         for(int i = 1; i < str.length() - 1 ; i++) {
             if(str.charAt(i) != ' ' || (str.charAt(i-1) == ' ' || str.charAt(i+1) == ' ')) {
-                s1 += String.valueOf(str.charAt(i));
+                newstring += String.valueOf(str.charAt(i));
             }
         }
         
         //Vérification se réalisant en fin de chaîne de caractère
         if(str.length() > 1) {
             if(str.charAt(str.length() - 1) != ' ' || str.charAt(str.length() - 2) == ' ') {
-                s1 += String.valueOf(str.charAt(str.length() - 1));        
+                newstring += String.valueOf(str.charAt(str.length() - 1));        
             }
         }
-        
-            
+                    
         return s1;
         
     }
