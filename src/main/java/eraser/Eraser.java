d87ae7bd33e4e7da6311e8ddd53e657f3363d4e4
Mazlai | マイ <57178136+Mazlai@users.noreply.github.com>
2022-06-10 23:56:01 +0200

    Update (javadoc) : Erase.java

--- src/main/java/eraser/Eraser.java
+++ src/main/java/eraser/Eraser.java
@@ -1,10 +1,17 @@
 package eraser;
 
 public class Eraser {
+    /** Permet de modifier la chaîne de caractère renseignée en paramètre
+     * @param IN str : la chaîne de caractère
+     * @return la nouvelle chaîne de caractère avec les traitements effectués
+     **/
     public static String erase(String str) {
+        //Déclarations
         StringBuilder strB = new StringBuilder(str);
             int taille = strB.length();
             int i = 0;
+        
+            //Vérification de la constitution de la chaîne de caractère
             for(int i  = 0; i < taille; i++) {
                 if(strB.charAt(i) == (char)32 && strB.charAt(Math.min(i+1, taille-2)) != (char)32 && strB.charAt(Math.max(i-1, 1)) != (char)32) {
                     strB.deleteCharAt(i);
